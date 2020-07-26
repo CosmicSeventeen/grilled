d3ec92dc781c965ad5d5ff525609b71b1ed7ac6f
Ithiel <cosmicking70@gmail.com>
2020-07-26 18:59:05 -0500

    06-26-2020 assignment

---
+++ grilled.py
@@ -0,0 +1,23 @@
+#stage 1 data inp
+one slice of velveeta cheese
+two slices of brioche bread
+skillet
+plate
+
+#stage 2 data processing
+
+go to a store (like walmart)  
+buy brioche bread 
+buy velveeta cheese 
+take two slices of bread out 
+take one slice of velveeta cheese
+place cheese onto a slice of bread
+place the other slice of bread on top
+heat the skillet for around 3 minutes on medium
+put the bread on the skillet 
+grill for 5 minutes on each side
+
+#stage3 results
+
+once completed, place the grilled cheese onto a plate 
+enjoy
