c6941b58085cf672c675608f36184cc22961f885
Ithiel <cosmicking70@gmail.com>
2020-08-12 18:49:44 -0500

    variables

---
+++ snack.py
@@ -0,0 +1,9 @@
+fav_snack = 'gummies'
+print('gummies' * 100)
+friend = input("please enter your favorite snack: ")
+friend = 'sour patch'
+print(fav_snack * 100, friend * 100)
+groceries = ['pots', 'pans', 'spoons', 'forks', 'knives', 'sour patch']
+groceries.index('sour pa
+fav_snack, friend = friend, fav_snack
+print(fav_snack, friend)
